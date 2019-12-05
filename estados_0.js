caae635e463648f2591a785710f6a2a7b3ed4d12
kennett01 <58481937+kennett01@users.noreply.github.com>
2019-12-05 14:44:05 -0600

    Add files via upload

--- estados_0.js
+++ estados_0.js
@@ -1,4 +1,4 @@
-var json_Simplified_0 = {
+var json_estados_0 = {
 "type": "FeatureCollection",
 "name": "Simplified_0",
 "crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
